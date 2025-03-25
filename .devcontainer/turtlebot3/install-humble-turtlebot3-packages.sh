eaf07fbc5752bb1b7ec634592db61aea4a585315
YABE.Kazuhiro <Kazuhiro.Yabe@gmail.com>
2025-03-25 10:48:57 +0900

    update scripts

---
+++ .devcontainer/turtlebot3/install-humble-turtlebot3-packages.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+apt-get install -y ros-humble-turtlebot3*
+apt-get install -y ros-humble-turtlebot3-simulations
+
---
+++ .devcontainer/turtlebot3/install-jazzy-turtlebot3-packages.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+apt-get install -y ros-jazzy-turtlebot3*
+apt-get install -y ros-jazzy-nav2-minimal-tb3-sim
+
