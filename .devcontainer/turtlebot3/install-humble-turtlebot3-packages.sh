30bed050b36f762d5a21b435cd83103f21f75fe8
YABE.Kazuhiro <k.yabe@qibitech.com>
2025-04-04 16:12:07 +0900

    support ros1 noetic

--- .devcontainer/turtlebot3/install-humble-turtlebot3-packages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-apt-get install -y ros-humble-turtlebot3*
-apt-get install -y ros-humble-turtlebot3-simulations
-
--- .devcontainer/turtlebot3/install-jazzy-turtlebot3-packages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-apt-get install -y ros-jazzy-turtlebot3*
-apt-get install -y ros-jazzy-nav2-minimal-tb3-sim
-
