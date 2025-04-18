2b6e76428c114dc52f9b98018896af2186e48977
YABE.Kazuhiro <k.yabe@qibitech.com>
2025-04-18 09:41:14 +0900

    update python package install script

--- .devcontainer/scripts/install_python_utils.sh
+++ .devcontainer/scripts/install_python_utils.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 set -eu
+source ~/.bashrc
 export PIPX_HOME=/home/${USER}/.local/share/pipx/venvs
-pipx install uv poetry isort flake8 black mypy ruff pyright
+PACKAGES="\
+    uv \
+    isort\
+    flake8\
+    black\
+    mypy\
+    ruff\
+    pyright \
+"
+
+for package in ${PACKAGES} ; do
+    pipx install ${package}
+done
