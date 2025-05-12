1b3ba273e25707fd3dacc0ccb23265d9bae41f4e
YABE.Kazuhiro <k.yabe@qibitech.com>
2025-05-12 18:30:02 +0900

    update scripts

--- utils/build-container.sh
+++ utils/build-container.sh
@@ -6,12 +6,14 @@ if [[ -f /.dockerenv ]] ; then
   exit 1
 fi
 
+ROS_DISTRO=${ROS_DISTRO:-jazzy}
 
 #CONTAINER_NAME=my_ros:latest
 CONTAINER_NAME=$(basename $(git rev-parse --show-toplevel))-$(git rev-parse --abbrev-ref HEAD):latest
 REPOSITORY_ROOT=$(git rev-parse --show-toplevel)
 
 docker build -t ${CONTAINER_NAME} \
+  --build-arg ROS_DISTRO=${ROS_DISTRO} \
   --build-arg USER_UID=$(id -u) \
   --build-arg USER_GID=$(id -g) \
   --build-arg DOCKER_GID=$(getent group docker | cut -d: -f3) \
